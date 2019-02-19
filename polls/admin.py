833d3ba006f2f58b04c1f4e6ce8913421120e013
GESION\LuNan.Liu <lunan.liu@gesion.net>
2019-02-19 10:41:27 +0800

    choice的admin在question一起显示

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    # 模型名称
+    model = Choice
+    # 额外最多一次性加三条
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question Content', {'fields': ['question_text'], 'classes': ['collapse']}),
+        # Date Information 字段介绍
+        # 'classes': ['collapse']为隐藏选项
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']})
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
