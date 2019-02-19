0a918d40a3fc6f3ad3efa347127de2cc0a217015
GESION\LuNan.Liu <lunan.liu@gesion.net>
2019-02-19 10:57:21 +0800

    精化admin显示页面

--- polls/admin.py
+++ polls/admin.py
@@ -3,8 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+# StackedInline显示在多行
+# class ChoiceInline(admin.StackedInline):
+#     # 模型名称
+#     model = Choice
+#     # 额外最多一次性加三条
+#     extra = 3
 
-class ChoiceInline(admin.StackedInline):
+
+# TabularInline显示在一行，还额外增加一个删除按钮
+class ChoiceInline(admin.TabularInline):
     # 模型名称
     model = Choice
     # 额外最多一次性加三条
@@ -12,6 +20,9 @@ class ChoiceInline(admin.StackedInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    # 在对象的更改列表页面上显示的字段名称元组
+    list_display = ('question_text', 'pub_date')
+
     fieldsets = [
         ('Question Content', {'fields': ['question_text'], 'classes': ['collapse']}),
         # Date Information 字段介绍
